f2fad0944ae4098685882d61c7a7542a2a89a6a6
Akash Saxena <akashsaxena5555@gmail.com>
2022-10-18 17:00:05 +0530

    refactor for custom error message

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-searchValue = document.querySelector('input')
+let searchValue = document.querySelector('input');
+const validityState = searchValue.validity;
 submitButton = document.querySelector('.submit')
 const d = new Date();
 city = document.querySelector('.city')
 country = document.querySelector('.country')
 time = document.querySelector('.date')
 temp = document.querySelector('.temp')
+let searchResult = document.querySelector('main');
+let imageResult = document.querySelector('.imageSlider');
 
 function tempConverter(Kelvin) {
     return (Kelvin - 273.15).toPrecision(4) + "° C"
@@ -16,17 +19,30 @@ function generateOutput(response) {
     time.textContent = "Date : " + d.toLocaleDateString();
     temp.textContent = "Temperature : " + tempConverter(response.main.temp);
 }
+function checkInputValidity(){
+    if (validityState.valueMissing) {
+        searchValue.setCustomValidity('You gotta fill this out, yo!');
+    }else{
+        searchValue.setCustomValidity(''); //IMPORTANT to reset the validity message like this.
+    }
+}
 submitButton.addEventListener('click',(event) => {
-    event.preventDefault()
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue.value}&appid=1333c570373880c9c466657806df0cf2`, {mode: 'cors'})
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(response) {
-        console.log(response);
-        generateOutput(response);
-      })
-    .catch(function(err){
-        console.log('error caught ', err)
-    });
+    checkInputValidity(); //Check Validity should be implemented here as the first step, and not as the else condition of the the following if condition. This is because setCustomValidity() method will cause checkValidity() method to return false.
+    if(searchValue.checkValidity()){
+        event.preventDefault();
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue.value}&appid=1333c570373880c9c466657806df0cf2`, {mode: 'cors'})
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(response) {
+            console.log(response);
+            generateOutput(response);
+            searchResult.style.display = 'flex';
+            imageResult.style.display = 'flex';
+          })
+        .catch(function(err){
+            console.log(err);
+        });
+    }
+    // event.preventDefault();
 })
